171f388451f485d84965344e38f8871914e063aa
niko <nayeomin@gmail.com>
2025-12-09 05:08:06 +0000

    Add product calculation

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ a = float(input("Enter the first number: "))
 b = float(input("Enter the second number: "))
 
 #calculating the difference of a and b
+sum = a + b
 difference = a - b
 
 #printing the result
+print("The sum is :", sum)
 print("The difference is : ", difference)
+print("The product is : ", a * b)
