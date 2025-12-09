ae7008de1c3d4d132d6b53328ab4021f86bb2f28
niko <nayeomin@gmail.com>
2025-12-09 04:59:27 +0000

    Add main.py for calculating sum

---
+++ main.py
@@ -0,0 +1,11 @@
+#main.py
+
+#getting the numbers
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+#calculating the difference of a and b
+difference = a - b
+
+#printing the result
+print("The difference is : ", difference)
